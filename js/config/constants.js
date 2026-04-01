5f4b2454594a2e7f30423f92f87eb98de32a607f
ghoose007 <ghosti007123@gmail.com>
2026-03-31 23:28:24 -0500

    Asset Version changes

--- js/config/constants.js
+++ js/config/constants.js
@@ -2,7 +2,7 @@
 // CONFIGURATION CONSTANTS
 // ==========================================
 
-const ASSET_VERSION = "107";
+const ASSET_VERSION = "108";
 
 const CHUNK_SIZE = 16;
 const CHUNK_HEIGHT = 256; 
